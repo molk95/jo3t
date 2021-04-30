0ddca0b1e0d7f2cdf93d4c35892b04d16c92e977
molk95 <molk.saouabi@gmail.com>
2021-04-30 19:47:12 +0200

    info

--- src/components/features/restaurents/components/restaurent-info.component.js
+++ src/components/features/restaurents/components/restaurent-info.component.js
@@ -1,4 +1,17 @@
 import React from "react";
 import { Text } from "react-native";
 
-export const restaurentInfo = () => <Text>restaurentInfo</Text>;
+export const restaurentInfo = ({ restaurent = {} }) => {
+  const {
+    name = "Some Restaurant",
+    icon,
+    photos = [
+      "https://media-cdn.tripadvisor.com/media/photo-s/15/c4/f0/44/la-joce-la-pizz-de-la.jpg",
+    ],
+    address = "100 random street",
+    isOpenNow = true,
+    rating = 3,
+    isClosedTemporarily,
+  } = restaurent;
+  return <Text>{name}</Text>;
+};
